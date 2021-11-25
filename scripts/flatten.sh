4a90c6b37b71a85e6091bd5311d007edcd6a5810
Haavi <javierortincervera@gmail.com>
2021-11-25 21:48:29 +0200

    Minor changes

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -5,3 +5,9 @@ for FILE in ./contracts/*.sol; do
     echo $file_name
     npx hardhat flatten $FILE > "./flatten/${file_name%.*}.txt"
 done
+
+for FILE in ./contracts/proxy/*.sol; do
+    file_name=$(basename "$FILE")
+    echo $file_name
+    npx hardhat flatten $FILE > "./flatten/${file_name%.*}.txt"
+done
